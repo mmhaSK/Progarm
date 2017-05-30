0f503bf675b04f593fe3952c6555d56b7bfb54e7
Damian Majercak <damian.majercak@gmail.com>
2017-05-30 16:30:02 +0200

    Add project files.

---
+++ App2/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Java.Lang;
+using System;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true, Icon = "@drawable/tatras_logo")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+            DoWorkAsync();
+
+        }
+
+        async void DoWorkAsync()
+        {
+            await Task.Delay(5000);
+            StartActivity(typeof(LoginActivity1));
+        }
+    }
+}
+
